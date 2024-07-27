d4b61925e8188e2d461893a267d31157d6752a70
Tue Do <tuedo.lm@gmail.com>
2024-07-27 16:23:45 -0700

    fix nextjs config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['example.com'], // Replace with your actual domains
+      deviceSizes: [320, 420, 768, 1024, 1200],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      formats: ['image/avif', 'image/webp'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+  
+  export default nextConfig;
+  
