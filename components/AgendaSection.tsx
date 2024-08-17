bfea3e049c12a1aa8f64ab795c8fa9e541233ba1
Tue Do <tuedo.lm@gmail.com>
2024-08-16 18:14:42 -0700

    commit new agenda section with detail for conference.

--- components/AgendaSection.tsx
+++ components/AgendaSection.tsx
@@ -41,52 +41,159 @@ const agendaItems = {
   ],
   "Saturday,August 17": [
     {
-      time: "9:00 AM - 10:00 AM",
-      title: "Check-in",
-      location: "Main Entrance",
-      description: "Get your badges and materials for the day.",
+      time: "9:00 AM - 9:45 AM",
+      title: "Check in & Registration",
+      location: "Sequoia Room",
+      description: "Check in and registration for the event.",
+      speakers: [],
+    },
+    {
+      time: "9:45 AM - 10:00 AM",
+      title: "Welcome by organizers",
+      location: "Sequoia Room",
+      description: "Kickoff speech from the organizers.",
       speakers: [],
     },
     {
       time: "10:00 AM - 10:15 AM",
-      title: "Welcome Speech from Organizers and Sponsors",
-      location: "Main Hall",
-      description: "Kickoff speech from our organizers and sponsors.",
+      title: "Keynote",
+      location: "Sequoia Room",
+      description: "Keynote speech.",
+      speakers: [
+        { name: "Trieu Trinh", title: "Keynote Speaker", image: "/speakers/trieutrinh-photo.jpg" },
+        { name: "Hung Hoang Nguyen", title: "Keynote Speaker", image: "/speakers/hungnguyen-photo.jpeg" },
+      ],
+    },
+    {
+      time: "10:15 AM - 11:45 AM",
+      title: "Panel Discussion: AI Beyond the Horizon",
+      location: "Sequoia Room",
+      description: "Panel discussion on AI: Beyond the Horizon.",
+      speakers: [
+        { name: "Trieu Trinh", title: "Panelist", image: "/speakers/trieutrinh-photo.jpg" },
+        { name: "Hung Hoang Nguyen", title: "Panelist", image: "/speakers/hungnguyen-photo.jpeg" },
+        { name: "Ray Nguyen", title: "Panelist", image: "/speakers/raynguyen-photo.jpg" },
+        { name: "Tuoc Luong", title: "Panelist", image: "/speakers/tuocluong-photo.jpg" },
+      ],
+    },
+    {
+      time: "11:45 AM - 12:15 PM",
+      title: "Lunch Break",
+      location: "Sequoia Room",
+      description: "Break for lunch.",
       speakers: [],
     },
     {
-      time: "10:15 AM - 11:15 AM",
-      title: "Panel Discussion",
-      location: "Main Hall",
-      description: "Discussion on the latest trends and innovations in tech.",
+      time: "12:15 PM - 1:00 PM",
+      title: "Speed Mentoring",
+      location: "Maple Room 2nd floor",
+      description: "Speed mentoring session.",
       speakers: [],
     },
     {
-      time: "11:15 AM - 12:00 PM",
-      title: "Panel Q&A",
-      location: "Main Hall",
-      description: "Q&A session with the panelists.",
-      speakers: [],
+      time: "1:00 PM - 2:00 PM",
+      title: "Workshop: Implementing LLM Virtual Assistants in Vietnam: Technological Challenges and Opportunities",
+      location: "Sequoia Room 1st floor",
+      description: "Workshop on implementing LLM virtual assistants in Vietnam.",
+      speakers: [
+        { name: "Hung Hoang Nguyen", title: "Workshop Speaker", image: "/speakers/hungnguyen-photo.jpeg" },
+      ],
     },
     {
-      time: "12:00 PM - 1:00 PM",
-      title: "Lunch",
-      location: "Dining Area",
-      description: "Enjoy a variety of delicious meals.",
-      speakers: [],
+      time: "1:00 PM - 2:00 PM",
+      title: "Workshop: How to bring AI from research to product",
+      location: "Maple Room 2nd floor",
+      description: "Workshop on transitioning AI from research to product.",
+      speakers: [
+        { name: "Sunny Hien Nguyen", title: "Workshop Speaker", image: "/speakers/sunny-photo.jpeg" },
+      ],
     },
     {
-      time: "1:00 PM - 5:00 PM",
-      title: "Workshops",
-      location: "Various Locations",
-      description: "Hands-on workshops on various tech topics.",
-      speakers: [],
+      time: "1:00 PM - 2:00 PM",
+      title: "Workshop: Optimizing the Robotic Mind: Cross-Industry Insights in AI Mobility",
+      location: "Sequoia Room 1st floor",
+      description: "Workshop on cross-industry AI mobility insights.",
+      speakers: [
+        { name: "Manh Nguyen", title: "Workshop Speaker", image: "/speakers/manhnguyen-photo.jpeg"},
+      ],
+    },
+    {
+      time: "2:00 PM - 3:00 PM",
+      title: "Workshop: Data Science: What's left in the age of AI and LLM?",
+      location: "Maple Room 2nd floor",
+      description: "Workshop on the current state of data science.",
+      speakers: [
+        { name: "Tuan Doan Nguyen", title: "Workshop Speaker", image: "/speakers/tuandoan-photo.jpg" },
+      ],
+    },
+    {
+      time: "2:00 PM - 3:00 PM",
+      title: "Workshop: Living safely in this AI area",
+      location: "Elm Room 2nd floor",
+      description: "Workshop on safe practices in the AI era.",
+      speakers: [
+        { name: "Thai Duong", title: "Workshop Speaker", image: "/speakers/thaiduong-photo.webp" },
+      ],
+    },
+    {
+      time: "2:00 PM - 3:00 PM",
+      title: "Workshop: The Hitchhiker's Guide to the Software Engineering IC Path",
+      location: "Sequoia Room 1st floor",
+      description: "Workshop on the software engineering individual contributor path.",
+      speakers: [
+        { name: "Hung Doan", title: "Workshop Speaker", image: "/speakers/hungdoan-photo.jpg" },
+      ],
+    },
+    {
+      time: "3:00 PM - 4:00 PM",
+      title: "Workshop: Race to AGI and the impact to fundamental AI research",
+      location: "Maple Room 2nd floor",
+      description: "Workshop on the race to AGI.",
+      speakers: [
+        { name: "Anh Nguyen", title: "Workshop Speaker", image: "/speakers/tuananhnguyen-photo.jpeg" },
+      ],
+    },
+    {
+      time: "3:00 PM - 4:00 PM",
+      title: "Workshop: What does a Product Manager do? How to become one?",
+      location: "Elm Room 2nd floor",
+      description: "Workshop on product management.",
+      speakers: [
+        { name: "James Pham", title: "Workshop Speaker", image: "/speakers/jamesphan-photo.jpg" },
+      ],
+    },
+    {
+      time: "3:00 PM - 4:00 PM",
+      title: "Workshop: How to use AI to improve developer productivity",
+      location: "Sequoia Room 1st floor",
+      description: "Workshop on using AI to boost developer productivity.",
+      speakers: [
+        { name: "Chau Vu", title: "Workshop Speaker", image: "/speakers/chauvu-photo.png" },
+      ],
+    },
+    {
+      time: "4:00 PM - 5:00 PM",
+      title: "Workshop: Big Tech or Startups: Differences and common traits",
+      location: "Maple Room 2nd floor",
+      description: "Workshop on comparing big tech and startups.",
+      speakers: [
+        { name: "Tuoc Luong", title: "Workshop Speaker", image: "/speakers/tuocluong-photo.jpg" },
+      ],
+    },
+    {
+      time: "4:00 PM - 5:00 PM",
+      title: "Workshop: Career Compass: Choose Your Path and Financial Planning",
+      location: "Elm Room 2nd floor",
+      description: "Workshop on career development and financial planning.",
+      speakers: [
+        { name: "Duc Pham", title: "Workshop Speaker", image: "/speakers/ducpham-photo.jpg" },
+      ],
     },
     {
       time: "5:00 PM - 6:00 PM",
-      title: "Closing Remarks, Raffle, and Networking",
-      location: "Main Hall",
-      description: "Final remarks, raffle draw, and networking opportunities.",
+      title: "Lucky Draw & Networking (Snacks provided)",
+      location: "Sequoia Room",
+      description: "End of day lucky draw and networking with snacks provided.",
       speakers: [],
     },
   ],
@@ -111,7 +218,7 @@ const AgendaSection = () => {
                   </div>
                   <h3 className="mt-4 text-xl font-bold text-neutral-800 dark:text-neutral-100">{item.title}</h3>
                   <p className="mt-2 text-neutral-600 dark:text-neutral-400 whitespace-pre-wrap">{item.description}</p>
-                  {/* {item.speakers.length > 0 && (
+                  {item.speakers.length > 0 && (
                     <div className="mt-4 flex space-x-4">
                       {item.speakers.map((speaker, speakerIdx) => (
                         <div key={speakerIdx} className="flex items-center space-x-2">
@@ -129,7 +236,7 @@ const AgendaSection = () => {
                         </div>
                       ))}
                     </div>
-                  )} */}
+                  )}
                 </div>
               </div>
             ))}
